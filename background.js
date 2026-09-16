dc51327cd1ec673aac6b3f085c23c1ad00fc9d59
andreo141 <andreo141mlg@gmail.com>
2026-09-16 02:41:12 +0200

    add routeDownloadFile logic (WIP)

--- background.js
+++ background.js
@@ -1,4 +1,22 @@
-function routeDownloadFile(file) {}
+let subDirs = ["looperman"]; // v1: should be populated in UI, eventually this will be a dictionary i think
+let overwrittenId;
+
+async function routeDownloadFile(file, item, delta) {
+  for (const dir of subDirs) {
+    if (file.includes(dir)) {
+      overwrittenId = delta.id;
+      try {
+        await browser.downloads.download({
+          url: `${item[0].url}`,
+          filename: `${dir}/${file}`,
+          conflictAction: "uniquify",
+        });
+      } catch (err) {
+        console.error("Failed to move local download to subdirectory");
+      }
+    }
+  }
+}
 
 async function listener(downloadDelta) {
   if (downloadDelta.state.current === "complete")
@@ -7,7 +25,16 @@ async function listener(downloadDelta) {
         id: downloadDelta.id,
       });
       const filename = downloadItem[0].filename.split("/").pop();
-      console.log("filename", filename);
+      const fullPath = downloadItem[0].filename;
+
+      for (const dir of subDirs) {
+        if (fullPath.includes(`/${dir}/`)) {
+          await browser.downloads.removeFile(overwrittenId);
+          return;
+        }
+      }
+
+      await routeDownloadFile(filename, downloadItem, downloadDelta);
     } catch (err) {
       console.error("Failed to search download");
     }
