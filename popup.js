4ee8fd01743ae91fb15848876c011ad64d56512f
andreo141 <andreo141mlg@gmail.com>
2026-09-21 03:59:19 +0200

    Add rule adding mechanism

--- popup.js
+++ popup.js
@@ -1,17 +1,39 @@
 const addRuleBtn = document.getElementById("add-rule-btn");
+const rulesList = document.getElementById("rules-list");
 
 const getRules = async () => {
   const data = await browser.storage.local.get("rules");
   return data.rules;
 };
 
+const addListElement = (rule) => {
+  const listElem = document.createElement("li");
+  const listText = document.createTextNode(rule);
+  listElem.appendChild(listText);
+  rulesList.appendChild(listElem);
+};
+
 async function addRuleHandler() {
   const rules = await getRules();
   const rule = document.getElementById("add-rule-input").value;
+  if (rule.trim() === "") return;
   rules.push(rule);
+  addListElement(rule);
   await browser.storage.local.set({ rules });
+  reset();
 }
 
 addRuleBtn.addEventListener("click", (e) => {
   addRuleHandler();
 });
+
+const populateExistingRules = (async () => {
+  const rules = await getRules();
+  rules.forEach((rule) => {
+    addListElement(rule);
+  });
+})();
+
+const reset = () => {
+  document.getElementById("add-rule-input").value = "";
+};
